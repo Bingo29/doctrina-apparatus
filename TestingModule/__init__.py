f276506936d343e2d63064aeb9f621af5b0ccc73
dmifka <dora.mifka@gmail.com>
2017-05-17 11:51:19 +0200

    Dorina_promjena

--- TestingModule/__init__.py
+++ TestingModule/__init__.py
@@ -2,10 +2,8 @@ import random
 import sys
 import os
 
-print("Dobar dan i dobrodošli!")
-print("We can do it!")
-print("Ljudi isprobajte malo GIT na ovoj datoteci. Ovo je samo za isprobavanje.")
-print("Nema Interneta")
+print("Novi kod")
+
 
 pi_tuple = (3,1,4,1,5,9)
 new_tuple = list(pi_tuple)
@@ -14,5 +12,3 @@ print(new_list)
 print(len(new_list))
 print(max(new_list))
 print(min(new_list))
-
-print("Proba proba") 
