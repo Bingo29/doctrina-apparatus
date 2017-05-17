1baa491f22f3d691431352602fe6f520449196fe
dmifka <dora.mifka@gmail.com>
2017-05-17 12:15:00 +0200

    DorinaNovaPromjena

--- TestingModule/__init__.py
+++ TestingModule/__init__.py
@@ -8,6 +8,8 @@ print("Novi kod")
 pi_tuple = (3,1,4,1,5,9)
 new_tuple = list(pi_tuple)
 new_list = tuple(new_tuple)
+print("jos jedna nova promjena")
+
 print(new_list)
 print(len(new_list))
 print(max(new_list))
