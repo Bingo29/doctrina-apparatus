c93349f38c549607768ffe218f7102fe2c52dbc6
Elena Petek <elpetek@student.math.hr>
2017-05-17 11:05:08 +0200

    Dodani fileovi za bazicno rjesenje 1
    Sve je locirano u datoteci BasicSolution1.py

---
+++ Solutions/BasicSolution1.py
@@ -0,0 +1,15 @@
+'''
+Created on 15. svi 2017.
+
+@author: Elena
+'''
+
+
+def fja(strText):
+    print(strText)
+    return [5, 3]
+    
+
+povratna = fja("proba")
+
+a = 5
--- TestingModule/__init__.py
+++ TestingModule/__init__.py
@@ -1,3 +1,16 @@
-print("Dobar dan i dobrodoöli!")
+import random
+import sys
+import os
+
+print("Dobar dan i dobrodo≈°li!")
 print("We can do it!")
 print("Ljudi isprobajte malo GIT na ovoj datoteci. Ovo je samo za isprobavanje.")
+print("Nema Interneta")
+
+pi_tuple = (3,1,4,1,5,9)
+new_tuple = list(pi_tuple)
+new_list = tuple(new_tuple)
+print(new_list)
+print(len(new_list))
+print(max(new_list))
+print(min(new_list))
