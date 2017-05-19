bb677400132e691da5751182339fb8c71ac6fbce
Elena Petek <elpetek@student.math.hr>
2017-05-19 13:11:11 +0200

    ucitavanje tablice

--- Solutions/BasicSolution1.py
+++ Solutions/BasicSolution1.py
@@ -6,6 +6,24 @@ Created on 15. svi 2017.
 
 import numpy as np
 import pandas as pd
+from test.test_asyncio.test_events import data_file
+from array import array
+data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
 
-data_file = pd.read_csv('train.csv')
-print(data_file[0:50])
+cols = 6
+rows = len(data)
+
+tablecolrow = []
+
+for j, c in enumerate(data):
+    tablecolrow.append([])
+    for i, r in enumerate(data[c]):
+        tablecolrow[j].append(r)
+
+for i in range(0, rows):
+    for j in range(0, 6):
+        print(tablecolrow[j][i], end=' ')
+    print("", end='\n')
+    
+    
+tablerowcol = nesto(tablecolrow)
