291c6959906ff94e02c8689075d19d529b92c7fc
Elena Petek <elpetek@student.math.hr>
2017-05-19 18:06:55 +0200

    Konacno tablica pripremljena za daljnje obrade
    Sljedeci korak je isprobati pitanje pretvoriti u vektor znacajki(niz
    brojeva). Dakle ovaj dio treba biti napravljen za pocetak samo na jednom
    pitanju kako bi se kasnije analogno moglo poopciti na sva pitanja.

--- Solutions/BasicSolution1.py
+++ Solutions/BasicSolution1.py
@@ -2,22 +2,36 @@ import numpy as np
 import pandas as pd
 from test.test_asyncio.test_events import data_file
 from array import array
+
+def okreniTablicu(tablicacolrow):
+    col = len(tablicacolrow)
+    row = len(tablicacolrow[0])
+    novaTablica = [[None] * col for x in range(0, row)]
+    for i in range(0, col):
+        for j in range(0, row):
+            novaTablica[j][i] = tablicacolrow[i][j]
+        
+    return novaTablica;
+
+
 data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
 
 cols = 6
 rows = len(data)
 
 tablecolrow = []
+tablerowcol = []
 
 for j, c in enumerate(data):
     tablecolrow.append([])
     for i, r in enumerate(data[c]):
         tablecolrow[j].append(r)
 
+print(tablecolrow[4][5])
+
+tablerowcol = okreniTablicu(tablecolrow)
+
 for i in range(0, rows):
     for j in range(0, 6):
-        print(tablecolrow[j][i], end=' ')
-    print("", end='\n')
-    
-    
-tablerowcol = nesto(tablecolrow)
+        print(tablerowcol[i][j], end=' ')
+    print("", end='\n')    
