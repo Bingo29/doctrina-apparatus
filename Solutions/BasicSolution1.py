7e1b2513e541ba94061252c3fda7bdab92b35648
Elena Petek <elpetek@student.math.hr>
2017-05-20 14:55:04 +0200

    Tablica spremna za daljnju obradu
    Maknuti svi importovi koji se ne koriste.

--- Solutions/BasicSolution1.py
+++ Solutions/BasicSolution1.py
@@ -1,16 +1,4 @@
-import numpy as np
 import pandas as pd
-from array import array
-
-def okreniTablicu(tablicacolrow):
-    col = len(tablicacolrow)
-    row = len(tablicacolrow[0])
-    novaTablica = [[None] * col for x in range(0, row)]
-    for i in range(0, col):
-        for j in range(0, row):
-            novaTablica[j][i] = tablicacolrow[i][j]        
-return novaTablica;
-
 
 data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
 
@@ -25,13 +13,10 @@ for j, c in enumerate(data):
     for i, r in enumerate(data[c]):
         tablecolrow[j].append(r)
 
-print(tablecolrow[4][5])
-
-tablerowcol = okreniTablicu(tablecolrow)
-
 for i in range(0, rows):
     for j in range(0, 6):
-        print(tablerowcol[i][j], end=' ')
-    print("", end='\n')    
+        print(tablecolrow[j][i], end=' ')
+    print("", end='\n')
 
-print(tablerowcol[4][5])
+# This takes less memory than np.transpose
+tablerowcol = list(zip(*tablecolrow))
