077c8db841b77f461ba11fc77062d2e0e5617591
Elena Petek <elpetek@student.math.hr>
2017-05-25 09:23:30 +0200

    Update tablice
    Kopirana tablica iz BagofWords. Spremno za daljnju obradu.

--- Solutions/Word2vec.py
+++ Solutions/Word2vec.py
@@ -1,1 +1,29 @@
 print("Ovdje pisite rjesenje za word2vec")
+
+import pandas as pd
+
+data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
+
+cols = 6
+rows = len(data)
+
+tablecolrow = []
+tablerowcol = []
+
+for j, c in enumerate(data):
+    tablecolrow.append([])
+    for i, r in enumerate(data[c]):
+        tablecolrow[j].append(r)
+
+# This takes less memory than np.transpose
+tablerowcol = list(zip(*tablecolrow))
+
+for i in range(0, rows):
+    for j in range(0, 6):
+        print(tablerowcol[i][j], end=' ')
+    print("", end='\n')
+
+    
+
+
+
