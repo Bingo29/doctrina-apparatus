92d722b13e8a3384db6abad4f17d350aa06b99a0
Elena Petek <elpetek@student.math.hr>
2017-05-25 09:10:42 +0200

    Data Cleaning and Text Preprocessing for Bag of words
    Promjene rađene na rečenicama, tj na 3. i 4. stupcu tablice.
    Maknuti punktacijski znakovi, velika slova pretvorena u mala, maknuti
    stop words, korišten lemmatization.
    Ovo je spremno za creating features from bag of words.

--- Solutions/BasicSolution1.py
+++ Solutions/BasicSolution1.py
@@ -1,4 +1,36 @@
 import pandas as pd
+import re
+import string
+
+from nltk.corpus import stopwords # Import the stop word list
+cachedStopWords = set(stopwords.words("english"))
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+#remove punctuation marks from question
+#returns lower case string
+def RemovePunctuationCharacters(strQuestion):
+    strRetval = str(strQuestion)
+    for char in string.punctuation:
+        strRetval = strRetval.replace(char, "")
+        
+    strRetval = re.sub(' +', ' ', strRetval)
+    return strRetval.lower()
+
+#removes unneccecery parts of questions in order to prepare it for bag of words
+#returns filtered table
+def FilterTable(tbTableRowCol):
+    tbRetval = tbTableRowCol
+    for i, row in enumerate(tbRetval):
+        tbRetval[i] = list(row)
+        
+    for i in range(0, len(tbTableRowCol)):
+        for j in range(0, len(tbTableRowCol[0])):
+            if j == 3 or j == 4:
+                punctuationlessQuestion = RemovePunctuationCharacters(tbTableRowCol[i][j]) 
+                tbRetval[i][j] = ' '.join([word for word in punctuationlessQuestion.split() if word not in cachedStopWords])
+                tbRetval[i][j] =" ".join([lemmatizer.lemmatize(i) for i in tbRetval[i][j].split()])
+    return tbRetval
 
 data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
 
@@ -13,10 +45,17 @@ for j, c in enumerate(data):
     for i, r in enumerate(data[c]):
         tablecolrow[j].append(r)
 
-for i in range(0, rows):
-    for j in range(0, 6):
-        print(tablecolrow[j][i], end=' ')
-    print("", end='\n')
-
 # This takes less memory than np.transpose
 tablerowcol = list(zip(*tablecolrow))
+
+tablerowcol = FilterTable(tablerowcol)
+
+for i in range(0, rows):
+    for j in range(0, 6):
+        print(tablerowcol[i][j], end=' ')
+    print("", end='\n')
+
+    
+
+
+
