21225b7d2eebe58f8442ce21cf9bab0cd49f90b5
Elena Petek <elpetek@student.math.hr>
2017-05-27 11:01:44 +0200

    Lovre, ovdje pisi rjesenje za doc2vec
    Ovdje je potrebno implementirati rjesenje doc2vec. Samo hrabro, nije tesko. Ucitavanje podataka je sredeno,  zato ne bi trebalo biti problema. Sretno :)

---
+++ Solutions/Doc2vec.py
@@ -0,0 +1,24 @@
+print("Ovdje pisite rjesenje za Doc2vec")
+
+import pandas as pd
+
+data = pd.read_csv('train.csv', sep=',', lineterminator="\n")
+
+cols = 6
+rows = len(data)
+
+tablecolrow = []
+tablerowcol = []
+
+for j, c in enumerate(data):
+    tablecolrow.append([])
+    for i, r in enumerate(data[c]):
+        tablecolrow[j].append(r)
+
+# This takes less memory than np.transpose
+tablerowcol = list(zip(*tablecolrow))
+
+for i in range(0, rows):
+    for j in range(0, 6):
+        print(tablerowcol[i][j], end=' ')
+    print("", end='\n')
